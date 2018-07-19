2caf5802ce456d8dafe2a3c09009eb803356cf59
firstpluz <kamolchanok.kon-hin@kingpower.com>
2018-07-19 11:14:59 +0700

    fixed scroll, fixed path

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,5 +1,29 @@
-window.scroll({ 
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth' 
-  });
+var smoothScrollTo = (function () {
+  var timer, start, factor;
+
+  return function (target, duration) {
+    var offset = window.pageYOffset,
+        delta  = target - window.pageYOffset; // Y-offset difference
+    duration = duration || 1000;              // default 1 sec animation
+    start = Date.now();                       // get start time
+    factor = 0;
+
+    if( timer ) {
+      clearInterval(timer); // stop any running animation
+    }
+
+    function step() {
+      var y;
+      factor = (Date.now() - start) / duration; // get interpolation factor
+      if( factor >= 1 ) {
+        clearInterval(timer); // stop animation
+        factor = 1;           // clip to max 1.0
+      } 
+      y = factor * delta + offset;
+      window.scrollBy(0, y - window.pageYOffset);
+    }
+
+    timer = setInterval(step, 10);
+    return timer; // return the interval timer, so you can clear it elsewhere
+  };
+}());
